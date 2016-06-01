ba7d42a7af2158fe85a32d130a06093275e88a13
wangxiuwen <wanxiuwen@tijiaamail.com>
2016-06-01 19:19:04 +0800

    the lastTime = nextTime is not work in IOS8, and it cause the animation  very fast.

--- requestAnimationFrame.js
+++ requestAnimationFrame.js
@@ -26,7 +26,8 @@ if (!Date.now)
         window.requestAnimationFrame = function(callback) {
             var now = Date.now();
             var nextTime = Math.max(lastTime + 16, now);
-            return setTimeout(function() { callback(lastTime = nextTime); },
+			lastTime = nextTime;
+            return setTimeout(function() { callback(); },
                               nextTime - now);
         };
         window.cancelAnimationFrame = clearTimeout;
